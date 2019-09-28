ba71c6fa9dc7a1a708859d5b5e657b7d75076871
NoahAGonzales <51267673+NoahAGonzales@users.noreply.github.com>
2019-09-28 17:01:42 -0500

    First commit

---
+++ src/graphql/queries.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getUser = `query GetUser($id: ID!) {
+  getUser(id: $id) {
+    id
+    name
+    description
+  }
+}
+`;
+export const listUsers = `query ListUsers(
+  $filter: ModelUserFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      name
+      description
+    }
+    nextToken
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = `subscription OnCreateUser {
+  onCreateUser {
+    id
+    name
+    description
+  }
+}
+`;
+export const onUpdateUser = `subscription OnUpdateUser {
+  onUpdateUser {
+    id
+    name
+    description
+  }
+}
+`;
+export const onDeleteUser = `subscription OnDeleteUser {
+  onDeleteUser {
+    id
+    name
+    description
+  }
+}
+`;

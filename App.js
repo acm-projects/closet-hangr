a1357aff31696d115ab85d35f287436b8ef34a2c
NoelRomero602 <55221303+NoelRomero602@users.noreply.github.com>
2019-10-20 16:51:57 -0500

    confirmation page

--- App.js
+++ App.js
@@ -1,68 +1,127 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, Text, View, TextInput, TouchableOpacity,Button,Alert } from 'react-native';
+import { Image, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 
 
+const styles= StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      backgroundColor: '#3199ce',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    fieldContainer: {
 
-export default class Login extends Component {
-  render() {
-    return(
-      <View style={styles.container}>
-        <View style = {styles.logoContainer}>
-          <Logo />
-          <Text style={styles.title}>CLOSET HANGR
-          </Text>
-        </View>
-        <View style = {styles.fieldContainer}>
-          <UserNameField/>
-          </View>
-          <View style = {styles.fieldContainer}>
-          <EmailField />
-          </View>
-          <View style = {styles.fieldContainer}>
-          <PasswordField />
-        </View>
-        <Button
-          title="Login"
+flexDirection :'column',
+     
+      padding:30,
+      height: 100,
+      width: 400,
+      textAlign: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      
+
+    },
+    logoContainer:
+    {
+      alignItems: 'center',
+      //justifyContent: 'center',
+      height: 300,
+
+    },
+
+    logo:
+    {
+      height: 100,
+      width: 180,
+      borderWidth: 15,
+      borderColor: '#96c3da',
+    },
+    regularText: {
+      fontFamily: 'Roboto',
+      fontSize: 11,
+      color: '#3199ce',
+      marginTop: 1,
+      padding: 10,
+      backgroundColor: '#FFF',
+      borderWidth: 4,
+      borderColor: '#96c3da',
+      
+    },
+
+    title: {
+      color: '#FFF',
+      fontFamily: 'Roboto',
+      fontSize: 23,
+      fontWeight: 'bold',
+      marginTop: 1,
+      width: 200,
+      justifyContent: 'center',
+      textAlign: 'center',
+    },
+    
+  }
+)
+
+
+
+export default class App extends React.Component {
+ 
+
+  
+  render (){
+   
+  return (
+    <View style={styles.container}>
+     
+      <Text style={styles.title}>Please Enter Confirmation Code
+         </Text>
+       
+  
+<View style = {styles.fieldContainer}>
+
+<ConfirmationCodeField />
+
+</View>
+
+<Button
+          title="Continue"
           color="#0693e3"
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
+          onPress={() => Alert.alert('Simple Button pressed')}
         />
-         <Button
-          title="Sign Up"
-          color="#f8bbd0"
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
-        />
-      </View>
-    );
+ </View>
+
+  );
   }
 }
 
-export class Background extends Component {
-  render() {
-    return(
-      <View style={styles.container}>
-        <Logo hanger/>
-      </View>
-
-    );
+export class ConfirmationCodeField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
   }
-}
 
-//This is the class for the Logo! Replace with the actual logo
-export class Logo extends Component {
   render() {
-    let pic = {
-      uri: 'https://cdn4.vectorstock.com/i/1000x1000/21/33/hanger-icon-white-on-the-blue-background-vector-3452133.jpg'
-    };//gonna crop this soon!! or replace with our own logo (png file)
     return (
-      <Image
-      style={styles.logo}
-      source={pic} />
+      <View style={styles.fieldContainer}>
+        <TextInput
+          style={styles.regularText}
+          placeholder="Confirmation code"
+          placeholderTextColor={'#2a78a0'}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        
+      </View>
     );
+
+    
   }
 }
 
-//Need a text field
+
 export class EmailField extends Component {
   constructor(props) {
     super(props);
@@ -75,8 +134,9 @@ export class EmailField extends Component {
         <TextInput
           style={styles.regularText}
     
-          placeholder="        Email        "
+          placeholder="Email        "
           placeholderTextColor={'#2a78a0'}
+          
           onChangeText={(text) => this.setState({text})}
           value={this.state.text}
         />
@@ -86,96 +146,6 @@ export class EmailField extends Component {
   }
 }
 
-export class PasswordField extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
 
-  render() {
-    return (
-      <View style={styles.fieldContainer}>
-        <TextInput
-          style={styles.regularText}
-          placeholder="    Password    "
-          placeholderTextColor={'#2a78a0'}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-        
-      </View>
-    );
-  }
-}
-export class UserNameField extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
 
-  render() {
-    return (
-      <View style={styles.fieldContainer}>
-        <TextInput
-          style={styles.regularText}
-          placeholder="     Username    "
-          placeholderTextColor={'#2a78a0'}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-        
-      </View>
-    );
-  }
-}
-const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      backgroundColor: '#3199ce',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    logoContainer: {
-      alignItems: 'center',
-      //justifyContent: 'center',
-      height: 300,
-    },
-    logo: {
-      height: 150,
-      width: 180,
-      borderWidth: 15,
-      borderColor: '#96c3da',
-    },
-    title: {
-      color: '#FFF',
-      fontFamily: 'Roboto',
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginTop: 10,
-      width: 200,
-      justifyContent: 'center',
-      textAlign: 'center',
-    },
-    fieldContainer: {
-      
-      margin: 2,
-      padding: 5.5,
-      height: 50,
-      width: 1000,
-      textAlign: 'center',
-      alignItems: 'center',
-      justifyContent: 'center',
-      
 
-    },
-    regularText: {
-      fontFamily: 'Roboto',
-      fontSize: 14,
-      color: '#3199ce',
-      padding: 10,
-      backgroundColor: '#FFF',
-      borderWidth: 4,
-      borderColor: '#96c3da',
-      
-    },
-})

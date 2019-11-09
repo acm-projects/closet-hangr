9e4ea7336684311881a438aa7ff37376292f6cc6
NoelRomero602 <55221303+NoelRomero602@users.noreply.github.com>
2019-11-08 22:55:16 -0600

    Add files via upload
    Settings page

--- App.js
+++ App.js
@@ -1,151 +1,200 @@
-import React, { Component } from 'react';
-import { Image, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native'
 
-
-const styles= StyleSheet.create(
-  {
-    container: {
-      flex: 1,
-      backgroundColor: '#3199ce',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    fieldContainer: {
-
-flexDirection :'column',
-     
-      padding:30,
-      height: 100,
-      width: 400,
-      textAlign: 'center',
-      alignItems: 'center',
-      justifyContent: 'center',
-      
-
-    },
-    logoContainer:
-    {
-      alignItems: 'center',
-      //justifyContent: 'center',
-      height: 300,
-
-    },
-
-    logo:
-    {
-      height: 100,
-      width: 180,
-      borderWidth: 15,
-      borderColor: '#96c3da',
-    },
-    regularText: {
-      fontFamily: 'Roboto',
-      fontSize: 11,
-      color: '#3199ce',
-      marginTop: 1,
-      padding: 10,
-      backgroundColor: '#FFF',
-      borderWidth: 4,
-      borderColor: '#96c3da',
-      
-    },
-
-    title: {
-      color: '#FFF',
-      fontFamily: 'Roboto',
-      fontSize: 23,
-      fontWeight: 'bold',
-      marginTop: 1,
-      width: 200,
-      justifyContent: 'center',
-      textAlign: 'center',
-    },
-    
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
   }
-)
 
+  onPress = () => {
+    this.setState({
+      count: this.state.count+1
+    })
+  }
 
-
-export default class App extends React.Component {
- 
-
-  
-  render (){
-   
-  return (
-    <View style={styles.container}>
-     
-      <Text style={styles.title}>Please Enter Confirmation Code
-         </Text>
+ render() {
+   return (
+     <View style={styles.container}>
+       <View style ={[styles.boarder]}>
+       <TouchableOpacity
+         style={styles.SettingsButton}
+         onPress={this.onPress}
+       >
+         <Text style = {[styles.title]}>>Settings: </Text>
+       </TouchableOpacity>
+       </View>
        
+       
+              <View style ={[styles.boarder]}>
+       <TouchableOpacity
+         style={styles.MyAccountbutton}
+         onPress={this.onPress}
+       >
+         <Text style = {[styles.titleAcount]} > MY ACCOUNT                                                                 > </Text>
+       </TouchableOpacity>
+       
+       </View>
+       <View style ={[styles.boarder]}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+         <Text style = {[styles.regularTitle]} > >Name     </Text>
+       </TouchableOpacity>
+      </View>
+      <View style ={[styles.boarder]}>
+<TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+         <Text style = {[styles.regularTitle]} > >Username    </Text>
+       </TouchableOpacity>       
+      </View>
+
+
+      <View style ={[styles.boarder]}>
+<TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+         <Text style = {[styles.regularTitle]} > >Birthday   </Text>
+       </TouchableOpacity>       
+       </View>
+      
+
+       <View style ={[styles.boarder]}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+         <Text style = {[styles.regularTitle]} > >Mobile Number   </Text>
+       </TouchableOpacity>       
+      
+       </View>
+       
+       <View style ={[styles.boarder]}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+         <Text style = {[styles.regularTitle]} > >Email   </Text>
+       </TouchableOpacity>  
+
+       </View>
+
+       <View style ={[styles.boarder]}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+         <Text style = {[styles.regularTitle]} > >Password  </Text>
+       </TouchableOpacity>  
+
+          </View>
+
+          <View style ={[styles.boarder]}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+         <Text style = {[styles.regularTitle]} > >WeatherApp  </Text>
+       </TouchableOpacity>  
+
+          </View>
+
+
+
+          <View style ={[styles.boarder]}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+         <Text style = {[styles.regularTitle]} > >Location </Text>
+       </TouchableOpacity>  
+
+          </View>
+          <View style ={[styles.boarder]}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+         <Text style = {[styles.regularTitle]} > >Notifications  </Text>
+       </TouchableOpacity>  
+
+          </View>
+
+       </View>
+
+    
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingHorizontal: 10
   
-<View style = {styles.fieldContainer}>
+  },
+  boarder: {
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: '#f5f5f5',
+  },
+  button: {
+    alignItems: 'flex-start',
+    backgroundColor: '#ffffff',
+    padding: 20
+  },
 
-<ConfirmationCodeField />
+  MyAccountbutton: {
+    alignItems: 'flex-start',
+    backgroundColor: '#f5f5f5',
+    padding: 10
+  },
 
-</View>
+  SettingsButton: {
+    alignItems: 'flex-start',
+    backgroundColor: '#ffffff',
+    padding: 30
+  },
 
-<Button
-          title="Continue"
-          color="#0693e3"
-          onPress={() => Alert.alert('Simple Button pressed')}
-        />
- </View>
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'mediumseagreen',
+   
+  },
+  regularTitle:{
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#696969'
+  },
 
-  );
-  }
-}
+  titleAcount: {
 
-export class ConfirmationCodeField extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
-
-  render() {
-    return (
-      <View style={styles.fieldContainer}>
-        <TextInput
-          style={styles.regularText}
-          placeholder="Confirmation code"
-          placeholderTextColor={'#2a78a0'}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-        
-      </View>
-    );
-
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'mediumseagreen',
     
+
+  },
+
+  countContainer: {
+    alignItems: 'center',
+    padding: 10
+  },
+  countText: {
+    color: '#FF00FF'
   }
-}
-
-
-export class EmailField extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
-
-  render() {
-    return (
-      <View style={styles.fieldContainer}>
-        <TextInput
-          style={styles.regularText}
-    
-          placeholder="Email        "
-          placeholderTextColor={'#2a78a0'}
-          
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-        
-      </View>
-    );
-  }
-}
-
-
+})
 
 
